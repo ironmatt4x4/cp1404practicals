0b95a52a9a2699ec08452d4a99e64cb0346aa4a1
Mattt <mattrc4x4@gmail.com>
2019-03-26 21:04:06 +1000

    add list_comprehensions and usernames

---
+++ prac_04/usernames.py
@@ -0,0 +1,6 @@
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+temp_user = input('enter your username: ')
+if temp_user in usernames:
+    print("Access Granted")
+else:
+    print("Access Denied")
