c7f8dcdc9dce6b0e09d2415ccf93319fefa39a76
Mattt <mattrc4x4@gmail.com>
2019-03-26 15:47:36 +1000

    work on prac 4

---
+++ prac_04/list_exercises.py
@@ -0,0 +1,15 @@
+print('enter the numbers')
+the_list = []
+list_total = 0
+for i in range(0, 5, 1):
+    temp = input("please enter a  number")
+    the_list.append(temp)
+
+for i in range(the_list):
+    list_total += i
+
+print('the first number is', the_list[0])
+print('the last number is', the_list[-1])
+print('the smallest number is', min(the_list))
+print('the largest number is', max(the_list))
+print('the average of the numbers is', list_total/len(the_list))
