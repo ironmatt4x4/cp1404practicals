bf4d89fdc818c95a0bef52bbc1d1d588a37f8c77
Mattt <mattrc4x4@gmail.com>
2019-04-08 14:08:19 +1000

    finish prac_05

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,16 +1,15 @@
 COLOURS = {'aliceblue': '#f0f8ff', 'antiquewhite': '#faebd7', 'antiquewhite1': '#ffefdb',
            'antiquewhite2': '#eedfcc', 'antiquewhite3': '#cdc0b0', 'antiquewhite4': '#8b8378',
            'aquamarine1': '#7fffd4', 'aquamarine2': '#76eec6', 'aquamarine4': '#458b74',
-           'beige': '#f5f5dc', 'bisque1': '#ffe4c4'}
+           'beige': '#f5f5dc', 'bisque1': '#ffe4c4', 'azure1': '#f0ffff', 'azure2': '#e0eeee',
+           'azure3': '#c1cdcd', 'azure4': '#838b8b'}
 while True:
     what_colour = input('Enter the colour').lower()
     try:
         if len(what_colour) < 1:
             print('thank you')
             break
-        else:
-            print(COLOURS[what_colour])
+        print(what_colour, 'is', COLOURS[what_colour])
+
     except KeyError:
         print('enter a valid colour')
-
-# 'bisque1': '#ffe4c4'
