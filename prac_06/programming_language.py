da94ec24c0568478c8380424a3580aff72ded204
Mattt <mattrc4x4@gmail.com>
2019-04-11 12:41:14 +1000

    start prac_06

---
+++ prac_06/programming_language.py
@@ -0,0 +1,9 @@
+class ProgrammingLanguage:
+    def __init__(self, year, typing, reflection):
+        self.year = year
+        self.typing = typing
+        self.reflection = reflection
+
+    def is_dynamic(self):
+        self.typing
+
